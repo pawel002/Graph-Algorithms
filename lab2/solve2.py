3624374b0b72b4f9390b204286d0d647af007f20
pawel002 <pawel.gandalf@gmail.com>
2022-11-17 14:21:06 +0100

    lab3

--- lab2/solve2.py
+++ lab2/solve2.py
@@ -60,51 +60,10 @@ def solve1_connectivity(V, L):
             graph[edge[1] - 1][edge[0] - 1] = edge[2]
         m = min(m, ford_flukerson(V, graph, adj_list, start, i))
     return m
-
-def stoer_wagner(V, L):
-    graph = [[] for _ in range(V)]
-    edges = [0 for i in range(V)]
-    visited = [False for i in range(V)]
-    s = [0 for i in range(V)]
-    que = [[0, 0]]
-
-    for (u, v, e) in L:
-        graph[u-1].append(v-1)
-        graph[v-1].append(u-1)
-    
-    for i in range(V):
-        while True:
-            u = heapq.heappop(que)[1]
-            if not visited[u]:
-                break
-        
-        s[i] = u
-        visited[u] = True
-        if i == V - 1:
-            final_connectivity = edges[u]
-
-        for v in graph[u]:
-            if visited[v]:
-                continue
-            edges[v] += 1
-            heapq.heappush(que, (-edges[v], v))
-    
-    curr_connectivity = 0
-    merged_weights = [0 for i in range(V)]
-    merged_visited = [False for i in range(V)]
-    for u in s[:-1]:
-        merged_visited[u] = True
-        curr_connectivity -= merged_weights[u]
-        for v in graph[u]:
-            if not merged_visited[v]:
-                merged_weights[v] += 1
-                curr_connectivity += 1
-        final_connectivity = min(curr_connectivity, final_connectivity)
-
-    return final_connectivity
     
 # tester
-def tester(func, path):
+def tester(func, time_limit, path):
+
     cum_time = 0
     tests_passed = 0
     tests_count = len(os.listdir(path))
@@ -123,6 +82,8 @@ def tester(func, path):
         if result == int(solution):
             tests_passed += 1
             print(f"{'TEST'}{i+1 : >3}{' PASSED. TIME: '}{stop - start :.2f}{'s. Test Name: '}{name}{'.'}")
+        elif result == "timeout":
+            print(f"{'TEST'}{i+1 : >3}{' FAILED. TIMEOUT ERR'}{'s. Test Name: '}{name}{'.'}")
         else:
             print(f"{'TEST'}{i+1 : >3}{' FAILED. TIME: '}{stop - start :.2f}{'s. Test Name: '}{name}{'.'}")
             print(f"{'Your solution: '}{result}{'.'}")
@@ -133,10 +94,6 @@ def tester(func, path):
     print(f"{'PASSED '}{tests_passed}{'/'}{tests_count}{'.'}")
     print(f"{'TESTS TOOK: '}{cum_time :.2f}{'s.'}")
     
-method = 2
-if method == 1:
-    tester(solve1_connectivity, "lab2/graphs/connectivity")
-else:
-    tester(stoer_wagner, "lab2/graphs/connectivity")
+tester(solve1_connectivity, 3, "lab2/graphs/connectivity")
 
 
---
+++ lab3/solve.py
@@ -0,0 +1,88 @@
+from unicodedata import name
+from dimacs import *
+import os
+import time
+import collections
+import heapq
+import signal
+
+(V, L) = loadDirectedWeightedGraph(os.path.abspath('lab2\\graphs\\connectivity\\clique5'))     # wczytaj graf
+
+def stoer_wagner(V, L):
+    graph = [[] for _ in range(V)]
+    edges = [0 for i in range(V)]
+    visited = [False for i in range(V)]
+    s = [0 for i in range(V)]
+    que = [[0, 0]]
+
+    for (u, v, e) in L:
+        graph[u-1].append(v-1)
+        graph[v-1].append(u-1)
+    
+    for i in range(V):
+        while True:
+            u = heapq.heappop(que)[1]
+            if not visited[u]:
+                break
+        
+        s[i] = u
+        visited[u] = True
+        if i == V - 1:
+            final_connectivity = edges[u]
+
+        for v in graph[u]:
+            if visited[v]:
+                continue
+            edges[v] += 1
+            heapq.heappush(que, (-edges[v], v))
+    
+    curr_connectivity = 0
+    merged_weights = [0 for i in range(V)]
+    merged_visited = [False for i in range(V)]
+    for u in s[:-1]:
+        merged_visited[u] = True
+        curr_connectivity -= merged_weights[u]
+        for v in graph[u]:
+            if not merged_visited[v]:
+                merged_weights[v] += 1
+                curr_connectivity += 1
+        final_connectivity = min(curr_connectivity, final_connectivity)
+
+    return final_connectivity
+    
+# tester
+def tester(func, time_limit, path):
+
+    cum_time = 0
+    tests_passed = 0
+    tests_count = len(os.listdir(path))
+
+    print("-" * 20 + "TESTS" + '-' * 20)
+
+    for i, graph in enumerate(os.listdir(path)):
+        (V, L) = loadDirectedWeightedGraph(os.path.abspath(path + "/" + graph))
+        name = graph.split("/")[-1]
+        solution = readSolution(os.path.abspath(path + "/" + graph))
+
+        start = time.time()
+        result = func(V, L)
+        stop = time.time()
+
+        if result == int(solution):
+            tests_passed += 1
+            print(f"{'TEST'}{i+1 : >3}{' PASSED. TIME: '}{stop - start :.2f}{'s. Test Name: '}{name}{'.'}")
+        elif result == "timeout":
+            print(f"{'TEST'}{i+1 : >3}{' FAILED. TIMEOUT ERR'}{'s. Test Name: '}{name}{'.'}")
+        else:
+            print(f"{'TEST'}{i+1 : >3}{' FAILED. TIME: '}{stop - start :.2f}{'s. Test Name: '}{name}{'.'}")
+            print(f"{'Your solution: '}{result}{'.'}")
+            print(f"{'Correct value: '}{solution}{'.'}")
+        cum_time += stop - start
+
+    print("-" * 50)
+    print(f"{'PASSED '}{tests_passed}{'/'}{tests_count}{'.'}")
+    print(f"{'TESTS TOOK: '}{cum_time :.2f}{'s.'}")
+    
+tester(stoer_wagner, 5,"lab3/graphs")
+
+
