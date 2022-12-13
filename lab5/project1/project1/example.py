a0e0efe82b1bc894a9aacf878531a912b5f6a388
pawel002 <pawel.gandalf@gmail.com>
2022-12-13 22:19:26 +0100

    g

---
+++ lab5/project1/project1/example.py
@@ -0,0 +1,12 @@
+from data import runtests
+
+def my_solve(N, M, K, base, wages, eq_cost):
+    print(f"Artystów: {N}, pokazów: {K}, występy: {K}")
+    for w in wages:
+        for (show, cost) in w:
+            # ...
+            pass
+    return 0
+
+runtests(my_solve)
+
